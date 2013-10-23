fe6cdfbf3db432d7e2308a4b9a237cded8148360
Chiel92 <ctje92@gmail.com>
2013-10-23 12:55:49 +0200

    framework in separate file

--- AntBrains.hs
+++ AntBrains.hs
@@ -1,55 +1,10 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module AntBrains where
 
-const1 = 0
-const2 = 37
+import AntSkull
 
 
--- A nice way to make our funtions
-class Compile r where
-    compile :: String -> r
-
-instance Compile (IO ()) where
-    compile s = putStrLn $ s
-
-instance (Compile r, Show a) => Compile (a -> r) where
-    compile s x = compile (s ++ " " ++ show x)
-
-
--- All functions are implemented using putStrLn's
-data SenseDir = Here | Ahead | LeftAhead | RightAhead deriving Show
-data Condition = Friend | Foe | FriendWithFood | FoeWithFood | Food | Rock | Marker Mark | FoeMarker | Home | FoeHome deriving Show
-data Turn     = Left | Right deriving Show
-type Mark     = Int
-
-sense :: SenseDir -> Int -> Int -> Condition -> IO ()
-sense = compile "Sense"
-
-mark :: Mark -> Int -> IO ()
-mark = compile "Mark"
-
-unmark :: Mark -> Int -> IO ()
-unmark = compile "Unmark"
-
-pickup :: Int -> Int -> IO ()
-pickup = compile "PickUp"
-
-drop :: Int -> IO ()
-drop = compile "Drop"
-
-turn :: Turn -> Int -> IO () 
-turn = compile "Turn"
-
-move :: Int -> Int -> IO ()
-move = compile "Move"
-
-random :: Int -> Int -> Int -> IO ()
-random p = compile "Flip" p
-
-
--- The actual program
 main :: IO ()
 main = do
-    move 1 2
-    pickup 3 5
+    move 0 1
+    move 1 0
 
