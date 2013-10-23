1e98e73869253bc5954b7705807f7f5a4c47b54b
Mattias <matheiusb@gmail.com>
2013-10-23 14:53:35 +0200

    Randomizer implemented using a binary tree. Sorry, it's the smartest thing I could come up with.

--- AntSkull.hs
+++ AntSkull.hs
@@ -2,9 +2,12 @@
 module AntSkull where
 
 
--- A nice way to make our funtions
+-- A nice way to print our funtions
 class Compile r where
     compile :: String -> r
+
+instance Compile String where
+    compile s = s
 
 instance Compile (IO ()) where
     compile s = putStrLn $ s
@@ -13,7 +16,7 @@ instance (Compile r, Show a) => Compile (a -> r) where
     compile s x = compile (s ++ " " ++ show x)
 
 
--- All functions are implemented using putStrLn's
+-- Some datatypes to make life more beautiful
 data SenseDir  = Here | Ahead | LeftAhead | RightAhead deriving Show
 data Condition = Friend | Foe | FriendWithFood | FoeWithFood
                | Food | Rock | Marker Mark | FoeMarker
@@ -21,6 +24,8 @@ data Condition = Friend | Foe | FriendWithFood | FoeWithFood
 data Turn      = Left | Right deriving Show
 type Mark      = Int
 
+
+-- The primitive functions
 sense :: SenseDir -> Int -> Int -> Condition -> IO ()
 sense = compile "Sense"
 
@@ -42,6 +47,28 @@ turn = compile "Turn"
 move :: Int -> Int -> IO ()
 move = compile "Move"
 
-random :: Int -> Int -> Int -> IO ()
-random p = compile "Flip" p
+rand :: Int -> Int -> Int -> IO ()
+rand = compile "Flip"
+
+
+-- Our extension functions
+
+-- Create a decent randomizer in terms of the Flip randomizer
+-- Gets its current line number, the percentage in [0, 100], and the two state parameters
+random :: Int -> Float -> Int -> Int -> IO ()
+random lineNr p k1 k2 = putStrLn $ rtree p 50 1 -- I NEED MY CURRENT LINE NUMBER!!!
+      where
+        rtree :: Float -> Float -> Int -> String
+        rtree p q d | close p q = compile "Flip" k1 k2
+                    | otherwise = if p < q
+                                  then (compile "Flip" (lineNr+d) k2) ++ "\n" ++ rtree p (q - getP (d+1)) (d+1)
+                                  else (compile "Flip" k1 (lineNr+d)) ++ "\n" ++ rtree p (q + getP (d+1)) (d+1)
+
+        -- Check if the current probability is close enough to the wanted probability
+        close :: Float -> Float -> Bool
+        close goalP currentP = abs (goalP - currentP) < 1
+
+        -- Get the probability corresponding to the depth of the tree
+        getP :: Int -> Float
+        getP d = 100 / (2 ** (fromIntegral d))
 
