42dd9571edfc5da9305c5adcaf164f8b8b17114b
Chiel92 <ctje92@gmail.com>
2013-10-24 12:21:50 +0200

    state counter implemented

--- AntSkull.hs
+++ AntSkull.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module AntSkull where
 
+import Control.Monad.State
 
--- A nice way to print our funtions
+
+-- A nice way to print our functions
 class Compile r where
     compile :: String -> r
 
 instance Compile String where
     compile s = s
 
-instance Compile (IO ()) where
-    compile s = putStrLn $ s
+instance Compile (StateT Int IO ()) where
+    compile s =
+      do
+        modify (+1)
+        liftIO $ putStrLn s
 
 instance (Compile r, Show a) => Compile (a -> r) where
     compile s x = compile (s ++ " " ++ show x)
@@ -26,32 +31,55 @@ type Mark      = Int
 
 
 -- The primitive functions
-sense :: SenseDir -> Int -> Int -> Condition -> IO ()
+sense :: SenseDir -> Int -> Int -> Condition -> StateT Int IO ()
 sense = compile "Sense"
 
-mark :: Mark -> Int -> IO ()
+mark :: Mark -> Int -> StateT Int IO ()
 mark = compile "Mark"
 
-unmark :: Mark -> Int -> IO ()
+unmark :: Mark -> Int -> StateT Int IO ()
 unmark = compile "Unmark"
 
-pickup :: Int -> Int -> IO ()
+pickup :: Int -> Int -> StateT Int IO ()
 pickup = compile "PickUp"
 
-drop :: Int -> IO ()
+drop :: Int -> StateT Int IO ()
 drop = compile "Drop"
 
-turn :: Turn -> Int -> IO ()
+turn :: Turn -> Int -> StateT Int IO ()
 turn = compile "Turn"
 
-move :: Int -> Int -> IO ()
+move :: Int -> Int -> StateT Int IO ()
 move = compile "Move"
 
-rand :: Int -> Int -> Int -> IO ()
+rand :: Int -> Int -> Int -> StateT Int IO ()
 rand = compile "Flip"
 
 
 -- Our extension functions
+
+random' :: Float -> Int -> Int -> StateT Int IO ()
+random' p k1 k2 =
+  do
+    n <- get
+    rand n k1 k2
+
+
+main :: IO ()
+main =
+  do
+    runStateT program 0
+    return ()
+
+program :: StateT Int IO ()
+program =
+  do
+    random' 3 0 0
+    random' 3 0 0
+    random' 3 0 0
+    random' 3 0 0
+    random' 3 0 0
+    random' 3 0 0
 
 -- Create a decent randomizer in terms of the Flip randomizer
 -- Gets its current line number, the percentage in [0, 100], and the two state parameters
