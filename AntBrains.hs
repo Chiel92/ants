50e9e01f3053c0c9deebfd6dd3d2455c6bc5d4aa
MattiasBeimers <matheiusb@gmail.com>
2013-10-26 19:01:44 +0200

    Fixed all the bugs

--- AntBrains.hs
+++ AntBrains.hs
@@ -22,13 +22,15 @@ program _Search = do
 
     -- We start by searching anything, food, enemies, whatever.               CURRENTLY FOOD ONLY
     search _Search _TellFood _TellFoehome
+
     -- After we found anything, lets go tell the others what we found
     tellFoehome _TellFoehome
-    tellFood _TellFood _GetFood _ReturnFood
+    tellFood _TellFood _GetFood _ReturnFood _StoreFood
+
     -- Now either continue to set a trap, or to get food
     getFood _GetFood _ReturnFood
-    returnFood _ReturnFood _GetFood
-    storeFood _StoreFood _GetFood
+    returnFood _ReturnFood _StoreFood
+    storeFood _StoreFood _GetFood _Defend _ReturnFood
     defend _Defend
 
 
@@ -53,8 +55,8 @@ tellFoehome _this = do
     move _this 0 0
 
 
-tellFood :: Entry -> Entry -> Entry -> M ()
-tellFood _this _GetFood _ReturnFood = do
+tellFood :: Entry -> Entry -> Entry -> Entry -> M ()
+tellFood _this _GetFood _ReturnFood _StoreFood = do
     _checkExistingMarker <- alloc
     _checkHome           <- alloc
     _dropFood            <- alloc
@@ -69,26 +71,32 @@ tellFood _this _GetFood _ReturnFood = do
     -- Check if we are home, if so, _StoreFood
     senseAdj _checkHome _StoreFood _randomWalk _randomWalk Home
 
-     -- Do the random walk
+     -- If we did not find home or another food marker, do the random walk
     randomMove _randomWalk _this
 
 
 getFood :: Entry -> Entry -> M ()
 getFood _this _ReturnFood = do
-    lnr <- get
-    senseAdjMoveAndNot (lnr+9) (lnr+13) (lnr+13) Food Home   -- 0:  IF    there is food (not on Home) and we moved to it
-    nextL $ \n -> pickup n (lnr+13)                          -- 9:  THEN  pickup the food
-    turnAround _RETURN_FOOD                                  -- 10:       turn around and bring the food to our home
-    biasedMove lnr                                           -- 13: ELSE  do one step of a random walk and go on with what we do
+    _pickUp         <- alloc
+    _turnAround     <- alloc
+    _tryFollowTrail <- alloc 
 
+    -- Check if we found food, if so, get it, turn around and _ReturnFood
+    senseAdjMoveAndNot _this _pickUp _randomWalk _randomWalk Food Home
+    pickup _pickUp _turnAround _tryFollowTrail
+    turnAround _turnAround _ReturnFood
 
-returnFood :: Entrt -> Entry -> M ()
-returnFood _this _GetFood = do
-    lnr <- get
-    nextL $ \n -> mark _FOOD n                             -- 0:  tell others our mark
-    senseAdjMove (lnr+7) (lnr+11) (lnr+11) Home            -- 1:  IF   an adjacent cell is my anthill and I moved there
-    nextL $ \n -> drop n                                   -- 7:  THEN drop the food
-    turnAround _GET_FOOD                                   -- 8:       turn around and return searching
-    biasedMove (lnr+29)                                    -- 11: ELSE do one step of the random walk
-    sense Here (lnr+7) lnr Home                            -- 29:      and check if we are accidentally home now (if so, drop and search, of not, try again)
+    -- If we didn't find food, follow the trail to the food
+    tryFollowTrail _tryFollowTrail (Marker _FOOD) _this
+    
+
+returnFood :: Entry -> Entry -> M ()
+returnFood _this _StoreFood = do
+    _tryFollowTrail <- alloc
+
+    -- Check if we found home, if so _StoreFood
+    senseAdj _this _StoreFood _tryFollowTrail _tryFollowTrail Home
+
+    -- If we didn't find home, follow the trail
+    tryFollowTrail _tryFollowTrail _this
 
